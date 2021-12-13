0a0717080d3956faf9c3570aa78f86a3e31f7ec3
keroro00 <82613217+keroro00@users.noreply.github.com>
2021-12-13 14:22:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -369,6 +369,14 @@ app.get('/database', (req,res) => {
 });
 
 
+app.get("/map", (req,res) => {
+	res.render("/home/developer/proj/map.ejs", {
+		lat:req.query.lat,
+		lon:req.query.lon,
+		zoom:req.query.zoom ? req.query.zoom : 15
+	});
+	res.end();
+});
 
 app.get('/create', (req,res) => {
     res.render('/home/developer/proj/create.ejs');
